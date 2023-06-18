7e5fb36be8ce9ca2b5696008b5c609030f415f06
zwep <sebharrevelt@gmail.com>
2023-06-18 16:41:39 +0200

    test

--- webpage/run.py
+++ webpage/run.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import os
 import datetime
 
 app = Flask(__name__, template_folder='template')
 
 @app.route("/")
 def hello():
-   return render_template('index.html')
+   file_to_csv = os.path.expanduser('~/data/arduino/remote_sensor.csv')
+   return render_template('index.html', file_to_csv=file_to_csv)
 
 
 if __name__ == "__main__":
